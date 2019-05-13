8087d4147a4f03493cba9a5afe52bccea903a7d0
Josie <josiekwheeler@gmail.com>
2019-05-13 09:54:17 -0400

    final final

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
-
+"use strict";
+//prompting the user to enter in their total budget and setting the other items to 0.
       var budget = {
         "Total": prompt("Please enter total budget", "200"),
         "Entertainment": 0,
@@ -7,13 +8,14 @@
         "Bills": 0,
         "Remaining": 0
       }
-  
+  //loading the current chart from google  charts
       google.charts.load('current', {
         'packages': ['corechart']
       });
+      //updating the google chart everytime data is entered 
       google.charts.setOnLoadCallback(updateAll);
 
-
+//creating a function that adds the value to the chart
       function buttonPressed(e) {
         event.preventDefault();
         if(document.getElementById("amount").value.length === 0){
@@ -34,10 +36,12 @@
             break;
 
         }
+        //sends all the data to the chart when new data is inputed 
         updateAll();
+        //resetting the input value to $0
         document.getElementById("myForm").reset();
       }
-
+        //calculating all the integers that are stored/displayed in the table 
       function updateAll() {
         budget["Remaining"] = budget["Total"] - budget["Entertainment"] - budget["Food"] - budget["Clothing"] - budget["Bills"];
 
@@ -47,7 +51,7 @@
         document.getElementById("clothingSpent").innerHTML = "$" + budget["Clothing"]
         document.getElementById("billsSpent").innerHTML = "$" + budget["Bills"]
 
-
+//creating a variable with an array that stores all the numbers that are being calculated 
         var dataArrayGen = [
           ['Budget', "Dollars"],
           ['Entertainment', budget["Entertainment"]],
@@ -56,10 +60,10 @@
           ['Bills', budget["Bills"]],
           ['Remaining', budget["Remaining"]]
         ]
-
+//storing the data array that was declared above for the chart
         var data = google.visualization.arrayToDataTable(dataArrayGen);
 
-        
+        // giving adjustable properties to the chart to give it some design 
         var options = {
           is3D: true,
           pieSliceText:  `value`,
@@ -67,15 +71,13 @@
           height:400,
           
         };
-
+//applying the data to the chart and adjusting the slice sizes
         var chart = new google.visualization.PieChart(document.getElementById('piechart'));
 
+        //displaying an error message when the user has gone over their budget
         google.visualization.events.addListener(chart, 'error', function (googleError) {
-          console.log(googleError);
-          // google.visualization.errors.removeError(googleError.id);
           let errorMessage = document.querySelector("#piechart div span", "#piechart div");
           errorMessage.innerText = "Oh no, you're over budget Mate!!!!!";
-          console.dir(errorMessage);
           errorMessage.style.fontSize = "30px";
           errorMessage.style.display = "flex";
           errorMessage.style.backgroundColor = "";
@@ -83,6 +85,7 @@
           errorMessage.style.padding = "24%";
  
       });
+      //calling all the data to actually display the chart
         chart.draw(data, options);
       }
  
